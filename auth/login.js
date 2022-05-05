dbeba8debd3cf6627c3ee3ce0518014719ab62a2
Gian Barzano <gianbarzano@gmail.com>
2022-05-05 18:14:26 -0300

    TCC - Iniciando projeto

---
+++ auth/login.js
@@ -0,0 +1,7 @@
+onBtnLogarClick = () => {
+    const enderecoAtualSplit = window.location.href.split('/');
+    enderecoAtualSplit.pop();
+    enderecoAtualSplit.pop();
+    let novoEndereco = enderecoAtualSplit.join('/') + '/pets/pet-lista.html';
+    window.location.href = novoEndereco;
+}
