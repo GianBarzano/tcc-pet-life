9990e369681a82a79ff7434e2e49eefcb1954bf2
Gian Barzano <gianbarzano@gmail.com>
2022-05-05 22:17:08 -0300

    Navegacao inferior e Registro Pet

---
+++ pets/pet-cadastro.js
@@ -0,0 +1,13 @@
+onBtnCadastrarClick = () => {
+    const enderecoAtualSplit = window.location.href.split('/');
+    enderecoAtualSplit.pop();
+    let novoEndereco = enderecoAtualSplit.join('/') + '/pet-lista.html';
+    window.location.href = novoEndereco;
+}
+
+onBtnVoltarClick = () => {
+    const enderecoAtualSplit = window.location.href.split('/');
+    enderecoAtualSplit.pop();
+    let novoEndereco = enderecoAtualSplit.join('/') + '/pet-lista.html';
+    window.location.href = novoEndereco;
+}
---
+++ pets/pet-lista.js
@@ -0,0 +1,14 @@
+onBtnLogoutClick = () => {
+    const enderecoAtualSplit = window.location.href.split('/');
+    enderecoAtualSplit.pop();
+    enderecoAtualSplit.pop();
+    let novoEndereco = enderecoAtualSplit.join('/') + '/index.html';
+    window.location.href = novoEndereco;
+}
+
+onBtnNovoPetClick = () => {
+    const enderecoAtualSplit = window.location.href.split('/');
+    enderecoAtualSplit.pop();
+    let novoEndereco = enderecoAtualSplit.join('/') + '/pet-cadastro.html';
+    window.location.href = novoEndereco;
+}
