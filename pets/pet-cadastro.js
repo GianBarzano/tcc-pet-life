90bdd1306554f37ff4f72cc569c9777e85885edf
Gian Barzano <gianbarzano@gmail.com>
2022-05-09 19:43:22 -0300

    Ajustes

--- pets/pet-cadastro.js
+++ pets/pet-cadastro.js
@@ -2,6 +2,63 @@ onPagePetCadastroLoad = () => {
     const compFoto = document.querySelector('.comp-input-img');
     
     inicializarInputImg(compFoto);
+    const urlVideo = 'https://media.istockphoto.com/videos/aerial-view-of-the-shore-of-the-beach-in-cancun-mexico-video-id1264047349';
+    gerarImagemFromVideo(urlVideo)
+        .then((res) => {
+            console.log("Resultado imagem", res)
+        }).catch(err => {
+            console.log("Erro conversão imagem", err);
+        })
+}
+
+gerarImagemFromVideo = (url, seekTo = 0.0) => {
+    console.log("getting video cover for file: ", url);
+    return new Promise((resolve, reject) => {
+        try {
+            // load the file to a video player
+            const videoPlayer = document.createElement('video');
+            videoPlayer.setAttribute('src', url);
+            videoPlayer.addEventListener('error', (ex) => {
+                reject("error when loading video file", ex);
+            });
+            // load metadata of the video to get video duration and dimensions
+            videoPlayer.addEventListener('loadedmetadata', () => {
+                // seek to user defined timestamp (in seconds) if possible
+                if (videoPlayer.duration < seekTo) {
+                    reject("video is too short.");
+                    return;
+                }
+                
+                // extract video thumbnail once seeking is complete
+                videoPlayer.addEventListener('seeked', () => {
+                    console.log('video is now paused at %ss.', seekTo);
+                    // define a canvas to have the same dimension as the video
+                    const canvas = document.createElement("canvas");
+                    canvas.width = videoPlayer.videoWidth;
+                    canvas.height = videoPlayer.videoHeight;
+                    // draw the video frame to canvas
+                    const ctx = canvas.getContext("2d");
+                    ctx.drawImage(videoPlayer, 0, 0, canvas.width, canvas.height);
+                    // return the canvas image as a blob
+                    ctx.canvas.toBlob(
+                        blob => {
+                            resolve(blob);
+                        },
+                        "image/jpeg",
+                        0.75 /* quality */
+                    );
+                });
+
+                // delay seeking or else 'seeked' event won't fire on Safari
+                setTimeout(() => {
+                    videoPlayer.currentTime = seekTo;
+                }, 200);
+            });
+            videoPlayer.load();
+        } catch (error) {
+            reject("Erro ao converter");
+        }
+    });
 }
 
 onBtnCadastrarClick = () => {
